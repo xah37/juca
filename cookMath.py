c75144ef343d1691a8923d4019f188034a825ba6
jch <jeff.henschel@protonmail.com>
2021-12-01 20:13:36 -0600

    finished cooking  conversion table

--- cookMath.py
+++ cookMath.py
@@ -1,3 +1,8 @@
+from rich.console import Console
+from rich.table import Table
+from rich import box
+
+
 # ADD SOME MORE COOKING MATH
 # MAYBE USE THE FUNC FOR C TO F OR F TO C
 
@@ -10,4 +15,41 @@ def cups_to_tbsp(n):
     return f"{16*n} tbsp"
 
 
-print(cups_to_tbsp(4))
+def print_conv_table():
+    table = Table(title="Volume Conversion Chart")
+
+    table.add_column("Tsp.", justify="right", style="cyan", no_wrap=True)
+    table.add_column("Tbsp.", justify="right", style="cyan", no_wrap=True)
+    table.add_column("Ounces", justify="center", style="red",)
+    table.add_column("Cups", justify="center", style="red", no_wrap=True)
+
+    table.add_row(
+        '3',
+        '1',
+        '1/2',
+        '1/16'
+    )
+    table.add_row(
+        '12',
+        '4',
+        '2',
+        '1/4'
+    )
+    table.add_row(
+        '24',
+        "8",
+        "4",
+        "1/2",
+    )
+    table.add_row(
+        '48',
+        '16',
+        '8',
+        '1',
+    )
+
+    console = Console()
+    console.print(table)
+
+
+print_conv_table()
